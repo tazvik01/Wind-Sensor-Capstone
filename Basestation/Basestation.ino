f202a2e988fd77af8254730b59e15dade2d48784
tazvik01 <tazvikziauddin@gmail.com>
2025-03-25 01:21:08 -0600

    added decoy

--- Basestation/Basestation.ino
+++ Basestation/Basestation.ino
@@ -1,54 +1,50 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-// Pin definitions
+
 const int csPin = 5;
 const int resetPin = 2;
 const int irqPin = 26;
 
 byte localAddress = 0xBB;
-byte destination = 0xFF;
+bool firstMessage = true;
 
 void setup() {
   Serial.begin(9600);
-
   LoRa.setPins(csPin, resetPin, irqPin);
   if (!LoRa.begin(915E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-
-  // Start in 'idle' mode; parsePacket() will handle reading.
-  // Don’t use LoRa.receive() or LoRa.onReceive() if you are using parsePacket().
+  Serial.println("LoRa Receiver Started...");
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize > 0) {
-    // Now read the first four bytes for address info
+    
     int recipient = LoRa.read();    
     byte sender = LoRa.read();      
     byte incomingMsgId = LoRa.read();
     byte incomingLength = LoRa.read();
 
-    // Read the rest of the data
+  
     String incoming = "";
     while (LoRa.available()) {
       incoming += (char)LoRa.read();
     }
 
- 
-    // if (incomingLength != incoming.length()) {
-    //   Serial.println("Error: message length does not match");
-    //   return;
-    // }
-    // if (recipient != localAddress && recipient != 0xFF) {
-    //   Serial.println("This message is not for me.");
-    //   return;
-    // }
-
-    // Print info
-    Serial.println("Message: " + incoming);
-    Serial.println();
+    if (firstMessage) {
+      Serial.print("Device 0x");
+      Serial.print(sender, HEX);
+      Serial.print(": ");
+      Serial.print(incoming);
+      firstMessage = false;
+    } else {
+      Serial.print(" | Device 0x");
+      Serial.print(sender, HEX);
+      Serial.print(": ");
+      Serial.print(incoming);
+    }
   }
 }
---
+++ decoy/decoy.ino
@@ -0,0 +1,133 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Adafruit_GPS.h>
+#include <HardwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <SoftwareSerial.h>
+
+Adafruit_BME280 bme;
+#define SEALEVELPRESSURE_HPA (1013.25)
+//windsensor connection
+#define RX_PIN 22
+#define TX_PIN 21
+//for the LoRa
+const int csPin = 5;
+const int resetPin = 2;
+const int irqPin = 26;
+//just a led 
+const int ledPin = 33;
+//for the wind sensor
+SoftwareSerial sensorSerial(RX_PIN, TX_PIN);
+unsigned long lastSent = 0;
+//every 1 second
+const unsigned long sendInterval = 1000;  // Send combined data every 1 second
+//just creating a hardware seiral object
+HardwareSerial mySerial(2);
+//same as winsensor right above creating a hardware serial for GPS 
+Adafruit_GPS GPS(&mySerial);
+//mountain time
+#define TIME_ZONE_OFFSET -7
+String outMessage;
+byte msgCount = 0;
+String contents = "";
+//address of the current device
+byte localAddress = 0xBB;
+//adress of the device you want to send it to
+byte destination = 0xFF;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  mySerial.begin(9600, SERIAL_8N1, 16, 17);
+  GPS.begin(9600);
+  bme.begin(0x76);
+  gps_initialization();
+  Serial.println("Initializing Lora..");
+  LoRa.setPins(csPin, resetPin, irqPin);
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void gps_initialization() {
+  mySerial.begin(9600, SERIAL_8N1, 16, 17);
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+  delay(1000);
+}
+
+float convertNMEADegMinToDecDeg(float val, char dir) {
+  int degrees = (int)(val / 100);
+  float minutes = val - (degrees * 100);
+  float decDeg = degrees + minutes / 60.0;
+  if (dir == 'S' || dir == 'W') {
+    decDeg = -decDeg;
+  }
+  return decDeg;
+}
+
+void printCoordinates() {
+  float lat = convertNMEADegMinToDecDeg(GPS.latitude, GPS.lat);
+  float lon = convertNMEADegMinToDecDeg(GPS.longitude, GPS.lon);
+  Serial.print("Latitude: ");
+  Serial.print(lat, 6);
+  Serial.print("+Longitude$");
+  Serial.println(lon, 6);
+  Serial.print("Altitude(m):");
+  Serial.println(GPS.altitude);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  char c = GPS.read();
+
+  if (GPS.newNMEAreceived()) {
+    if (GPS.parse(GPS.lastNMEA())) {
+      int localHour = GPS.hour + TIME_ZONE_OFFSET;
+      if (localHour < 0) localHour += 24;
+      if (localHour >= 24) localHour -= 24;
+    }
+  }
+
+  unsigned long now = millis();
+  if (now - lastSent >= sendInterval) {
+    sendCombinedData();
+    lastSent = now;
+  }
+}
+
+void sendCombinedData() {
+  float lat = convertNMEADegMinToDecDeg(GPS.latitude, GPS.lat);
+  float lon = convertNMEADegMinToDecDeg(GPS.longitude, GPS.lon);
+  float alt = GPS.altitude;
+
+  float temperature = bme.readTemperature();
+  float pressure = bme.readPressure() / 100.0F;
+  float humidity = bme.readHumidity();
+
+  String combinedMessage = String("Lat2:") + String(lat, 6) + 
+                           ",Lon2:" + String(lon, 6) + 
+                           ",Alt2:" + String(alt, 2) + 
+                           ",Temp:" + String(temperature, 2) +
+                           ",Pressure:" + String(pressure, 2) +
+                           ",Humidity:" + String(humidity, 2);
+
+  sendMessage(combinedMessage);
+
+  Serial.println(combinedMessage);
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();
+  LoRa.write(destination);
+  LoRa.write(localAddress);
+  LoRa.write(msgCount);
+  LoRa.write(outgoing.length());
+  LoRa.print(outgoing);
+  LoRa.endPacket();
+  msgCount++;
+}
